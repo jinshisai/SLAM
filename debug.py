1214f73bd10ad605817133dcd3cb0e7bada4dc3f
jinshisai <jn.insa.sai@gmail.com>
2022-02-17 18:00:57 +0800

    debug

--- debug.py
+++ debug.py
@@ -6,35 +6,37 @@ from pvfit import PVFit
 
 def main():
 	# ------- input -------
-	fitsfile= 'testfits/l1489.c18o.contsub.gain01.rbp05.mlt100.cf15.pbcor.pv69d.fits'
-
-	outname = 'test'
-	rms     = 5.1e-3 # Jy/beam
-	thr     = 6.     # 6 sigma
+	fitsfile = 'testfits/l1489.c18o.contsub.gain01.rbp05.mlt100.cf15.pbcor.pv69d.fits'
 	vsys    = 7.3
 	incl    = 73.
+	pa      = 69.
+	fitsfile = 'testfits/tmc1a.c18o.contsub.rbp05.mlt100.clean.majpv.fits'
+	vsys    = 6.4
+	incl    = 65
+	pa      = 72
+	fitsfile = 'testfits/l1527.c18o.contsub.rbp05.mlt100.clean.majpv.fits'
+	vsys    = 5.8
+	incl    = 85
+	pa      = 0.
+
+	outname = 'test'
+	rms     = 5.e-3 # Jy/beam
+	thr     = 6.     # 6 sigma
 	dist    = 140.
 	clevels = np.array([-3,3,6,9,12,15,20,25,30])*rms
 	xlim    = [-5, 5] # arcsec
-	vlim    = [2, 12.5]
-	Mlim    = [1, 2]
+	vlim    = []
+	Mlim    = []
 	# ---------------------
 
 
 	# -------- main --------
-	impv = PVFit(fitsfile, rms, vsys, dist, pa=69.)
-	impv.get_edgeridge(outname, thr=thr, xlim=xlim, vlim=vlim, Mlim=Mlim, mode='gauss', incl=incl)
+	impv = PVFit(fitsfile, rms, vsys, dist, pa=pa)
+	impv.get_edgeridge(outname, thr=thr, xlim=xlim, vlim=vlim, Mlim=Mlim, mode='gauss', incl=incl,
+		use_velocity=False)
 	impv.plotresults_pvdiagram(clevels=clevels)
 	impv.plotresults_rvplane()
 	#print(impv.results_sorted)
-
-	#print(impv.results)
-	#impv.pvfit_xcut(outname, rms, thr)
-	#impv.plotresults_onpvdiagram(outname='test_xcut', clevels=clevels, colors=['r'])
-
-	#impv = PVFit(fitsfile) # just to reflesh results recorded above
-	#impv.pvfit_vcut(outname, rms, thr)
-	#impv.plotresults_onpvdiagram(outname='test_vcut', clevels=clevels, colors=['r'])
 	# ----------------------
 
 
