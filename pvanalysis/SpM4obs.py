11b32564368b68eba405ae2ba6aa9e67cc36bc43
Yusuke Aso <98569855+yusukeaso-astron@users.noreply.github.com>
2022-09-01 16:29:17 +0900

    FITS outputs for the deconvolution.

--- pvanalysis/SpM4obs.py
+++ pvanalysis/SpM4obs.py
@@ -1,109 +1,96 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import differential_evolution as difevo
-from scipy.interpolate import interp1d
+from astropy.io import fits
+from astropy import wcs
 
 from pvanalysis.pvfits import Impvfits
 
 
 rms = 1.4e-3
 thre = 3
-pa = 0
-fitsdata = Impvfits('/Users/yusukeaso/Desktop/IRAS16253_SBLB_12COh_robust_0.5.pvmajor.fits', pa=pa)
-answer = np.squeeze(fitsdata.data)
-bmaj, bmin, bpa = fitsdata.beam
-phi = np.arctan(bmin / bmaj)
-dpa = np.abs(np.radians(bpa - pa))
-r = np.cos(2*phi) * np.sin(2*dpa) / 2.
-#print(f'cross term ratio: {r:.2f}')
-xaxis = fitsdata.xaxis
-vaxis = fitsdata.vaxis
-dv = vaxis[1] - vaxis[0]
-dx = xaxis[1] - xaxis[0]
-res_off = fitsdata.res_off
-gbeam = np.exp2(-4 * (xaxis / res_off)**2)
-beampix = res_off / dx
-#print(f'res_off = {beampix:.1f} pixels')
-
-question = answer.copy()
-
+pa = 113
+dist = 139
 vsys = 4
-i0 = np.argmin(np.abs(xaxis + 70/139))
-i1 = np.argmin(np.abs(xaxis - 70/139)) + 1
-j0 = np.argmin(np.abs(vaxis - vsys + 6))
-j1 = np.argmin(np.abs(vaxis - vsys - 6)) + 1
-xaxis, vaxis = xaxis[i0:i1], vaxis[j0:j1] - vsys
-question, answer = question[j0:j1, i0:i1], answer[j0:j1, i0:i1]
+xmax = 70
+vmax = 6
+fitsfile = '/Users/yusukeaso/Desktop/IRAS16253_SBLB_12COh_robust_0.5.pvmajor.fits'
+fitsdata = Impvfits(fitsfile, pa=pa)
+question = np.squeeze(fitsdata.data)
+xaxis = fitsdata.xaxis * dist
+vaxis = fitsdata.vaxis - vsys
+res_off = fitsdata.res_off * dist
+gbeam = np.exp2(-4 * (xaxis / res_off)**2)
+
+i0 = np.argmin(np.abs(xaxis + xmax))
+i1 = np.argmin(np.abs(xaxis - xmax)) + 1
+j0 = np.argmin(np.abs(vaxis + vmax))
+j1 = np.argmin(np.abs(vaxis - vmax)) + 1
+xaxis, vaxis = xaxis[i0:i1], vaxis[j0:j1]
+question = question[j0:j1, i0:i1]
 gbeam = np.exp2(-4 * (xaxis / res_off)**2)
 beamarea = np.sum(gbeam)
 cmpthre = thre * rms / beamarea
 
-ngroup = 10
-def chisq(p, obs, l1, l2, l3, i_cve, mode=''):
+def chisq(p, obs, l1, l2):
     obscut = np.where(obs > 0, obs, obs * 0)
     obsarea = np.sum(obscut)
     if obsarea == 0:
         return 1000
     l1_org = obsarea / beamarea
     l2_org = np.sum(np.abs(obs[:-1] - obs[1:])**2) / beamarea**2
-    m = xaxis - np.average(xaxis, weights=obscut)
-    l3_org = np.average(m**2, weights=obscut) - beampix**2
-    l3_org = l3_org.clip(1, None)
     c0 = (np.convolve(p, gbeam, mode='same') - obscut) / rms
-    nmax = len(obs) // ngroup
-    if mode == 'pre':
-        r = list(range(ngroup))
-        r.remove(i_cve)
-        c0 = np.mean([np.abs(c0[j:nmax*ngroup:ngroup])**2 for j in r])             / (ngroup - 1) / nmax
-    elif mode == 'cve':
-        c0 = np.mean(np.abs(c0[i_cve:nmax*ngroup:ngroup])**2)
-    else:
-        c0 = np.mean(np.abs(c0)**2)
+    c0 = np.mean(np.abs(c0)**2)
     c1 = l1 * np.sum(np.abs(p)) / l1_org
     c2 = l2 * np.sum(np.abs(p[:-1] - p[1:])**2) / l2_org
-    m = xaxis - np.average(xaxis, weights=obscut)
-    c3 = np.average(m**2, weights=obscut)
-    c3 = l3 * c3.clip(1e-10, None) / l3_org
-    c = (c0 + c1 + c2 + c3) / (1 + l1 + l2 + l3)
+    c = (c0 + c1 + c2) / (1 + l1 + l2)
     return c
 
-a = np.max(question) * 2
-b = np.min(question) * 2 * 0
-bounds = [[b, a]] * len(xaxis)
-        
-deconv = []
-l1, l2, l3 = 0.5, 0.01, 0.6 * 0
-#l1, l2, l3 = 0, 0.1, 0.6
+bounds = [[0, np.max(question) * 2]] * len(xaxis)
+
+l1, l2 = 0.5, 0.01
 popsize = 15
-#print(len(xaxis), 'pixels')
-for v, y, p in zip(vaxis, question, answer):
-    #print(f'{v:.3f} km/s')
+
+deconv = []
+for v, y in zip(vaxis, question):
     if np.all(y < 0):
         deconv.append(y * 0)
         continue
     init = [y / beamarea] * popsize
-    res = difevo(chisq, bounds=bounds, args=[y, l1, l2, l3, 0, ''],
+    res = difevo(chisq, bounds=bounds, args=[y, l1, l2],
                  maxiter=int(1e6), tol=1e-6, popsize=popsize,
                  init=init, workers=1) #, updating='deferred')
-    c = chisq(p, y, l1, l2, l3, 0, '')
-    #print(f'{res.fun:.3f} {res.fun / c:.3f}')
     deconv.append(res.x)
 deconv = np.array(deconv)
 conv = np.array([np.convolve(d, gbeam, mode='same') for d in deconv])
+deconv_pad = np.squeeze(fitsdata.data) * 0
+deconv_pad[j0:j1, i0:i1] = deconv
+###########################################################
 
-xaxis = xaxis / res_off
+w = wcs.WCS(naxis=2)
+hdu = fits.PrimaryHDU(deconv_pad, header=w.to_header())
+h = fits.open(fitsfile)[0].header
+for k in h.keys():
+    if not ('COMMENT' in k or 'HISTORY' in k):
+        hdu.header[k] = h[k]
+hdu.header['BUNIT'] = 'Jy/pixel'
+hdu = fits.HDUList([hdu])
+hdu.writeto('SpM.fits', overwrite=True)
 
+###########################################################
 levels = np.arange(1, 10) * 6 * rms
 levels = np.sort(np.r_[-levels, levels])
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 c = ax.pcolormesh(xaxis, vaxis, question, shading='nearest',
-                  cmap='jet', vmin=0)
+                  cmap='viridis', vmin=0)
 fig.colorbar(c, ax=ax, label='Jy/beam')
 ax.contour(xaxis, vaxis, question, colors='w',
            levels=levels, linewidths=1)
-ax.set_xlabel('Position (beam)')
+ax.set_xlabel('Position (au)')
 ax.set_ylabel('Velocity (km/s)')
+ax.set_xlim(-xmax, xmax)
+ax.set_ylim(-vmax, vmax)
 fig.savefig('q_SpM.png')
 #plt.show()
 
@@ -112,12 +99,14 @@ levels = np.sort(np.r_[-levels, levels])
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 c = ax.pcolormesh(xaxis, vaxis, conv, shading='nearest',
-                  cmap='jet', vmin=0)
+                  cmap='viridis', vmin=0)
 fig.colorbar(c, ax=ax, label='Jy/beam')
 ax.contour(xaxis, vaxis, conv, colors='w',
            levels=levels, linewidths=1)
-ax.set_xlabel('Position (beam)')
+ax.set_xlabel('Position (au)')
 ax.set_ylabel('Velocity (km/s)')
+ax.set_xlim(-xmax, xmax)
+ax.set_ylim(-vmax, vmax)
 fig.savefig('c_SpM.png')
 #plt.show()
 
@@ -126,81 +115,29 @@ levels = np.sort(np.r_[-levels, levels])
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 c = ax.pcolormesh(xaxis, vaxis, (question - conv) / rms, shading='nearest',
-                  cmap='jet', vmin=-3, vmax=3)
+                  cmap='viridis', vmin=-3, vmax=3)
 fig.colorbar(c, ax=ax, label='sigma')
 ax.contour(xaxis, vaxis, question - conv, colors='w',
            levels=levels, linewidths=1)
-ax.set_xlabel('Position (beam)')
+ax.set_xlabel('Position (au)')
 ax.set_ylabel('Velocity (km/s)')
+ax.set_xlim(-xmax, xmax)
+ax.set_ylim(-vmax, vmax)
 fig.savefig('qvsc_SpM.png')
 #plt.show()
 
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
-c = ax.pcolormesh(xaxis, vaxis, answer, shading='nearest',
-                  cmap='jet')
-fig.colorbar(c, ax=ax, label='Jy/pixel')
-levels = 2**np.arange(10) * cmpthre
-levels = np.sort(np.r_[-levels, levels])
-ax.contour(xaxis, vaxis, answer, colors='w',
-           levels=levels, linewidths=1)
-ax.set_xlabel('Position (beam)')
-ax.set_ylabel('Velocity (km/s)')
-fig.savefig('a_SpM.png')
-#plt.show()
-
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
 c = ax.pcolormesh(xaxis, vaxis, deconv, shading='nearest',
-                  cmap='jet', vmin=0)
+                  cmap='viridis', vmin=0)
 fig.colorbar(c, ax=ax, label='Jy/pixel')
 levels = np.arange(1, 10) * 3 * cmpthre
 levels = np.sort(np.r_[-levels, levels])
 ax.contour(xaxis, vaxis, deconv, colors='w',
            levels=levels, linewidths=1)
-ax.set_xlabel('Position (beam)')
+ax.set_xlabel('Position (au)')
 ax.set_ylabel('Velocity (km/s)')
+ax.set_xlim(-xmax, xmax)
+ax.set_ylim(-vmax, vmax)
 fig.savefig('d_SpM.png')
 #plt.show()
-
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
-c = ax.pcolormesh(xaxis, vaxis, answer - deconv, shading='nearest',
-                  cmap='jet')
-fig.colorbar(c, ax=ax, label='Jy/pixel')
-levels = 2**np.arange(10) * cmpthre
-levels = np.sort(np.r_[-levels, levels])
-ax.contour(xaxis, vaxis, answer - deconv, colors='w',
-           levels=levels, linewidths=1)
-ax.plot(vaxis * 0 - 1, vaxis, '--w')
-ax.set_xlabel('Position (beam)')
-ax.set_ylabel('Velocity (km/s)')
-fig.savefig('avsd_SpM.png')
-#plt.show()
-
-xnew = np.linspace(xaxis[0], xaxis[-1], (len(xaxis) - 1) * 10 + 1)
-result = []
-for a, d, c, v in zip(answer, deconv, question, vaxis):
-    wa = wd = wc = 0
-    wa = xaxis[(a > 0) * (xaxis < 1.)]
-    wa = np.nan if len(wa) == 0 else wa[-1]
-    ynew = interp1d(xaxis, d, kind='cubic')(xnew)
-    g = ynew[:-1] - ynew[1:]
-    g[xnew[:-1] > 1.] = 0
-    wd = xnew[np.argmax(g)]
-    #wd = xnew[(ynew > cmpthre) * (xnew < 1.5)]
-    #wd = np.nan if len(wd) == 0 else wd[-1]
-    ynew = interp1d(xaxis, c, kind='cubic')(xnew)
-    wc = xnew[(ynew > rms * thre) * (xnew < 1.)]
-    wc = np.nan if len(wc) == 0 else wc[-1]
-    w = 3**(1 - (np.abs(v / dv) - 0.5) / 4.)
-    w = 0 if w < 0.32 else w
-    if w > 0:
-        r = (wc - wd) / w
-        result.append([int(round(1 / rms)), w, wc, wd])
-result = np.array(result)
-result = np.concatenate((result[:9], result[-1:-10:-1]), axis=0)
-with open('/Users/yusukeaso/Desktop/rec_SpM.dat', 'a') as f:
-    np.savetxt(f, result)
-#for r in result:
-#    print(f'{r[0]:.2f} {r[1]:.2f} {r[2]:.2f}')
--- pvanalysis/clean4obs.py
+++ pvanalysis/clean4obs.py
@@ -1,43 +1,36 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import differential_evolution as difevo
-from scipy.interpolate import interp1d
+from astropy.io import fits
+from astropy import wcs
 
 from pvanalysis.pvfits import Impvfits
 
 
 rms = 1.4e-3
 thre = 3
-pa = 0
-fitsdata = Impvfits('/Users/yusukeaso/Desktop/IRAS16253_SBLB_12COh_robust_0.5.pvmajor.fits', pa=pa)
-answer = np.squeeze(fitsdata.data)
-bmaj, bmin, bpa = fitsdata.beam
-phi = np.arctan(bmin / bmaj)
-dpa = np.abs(np.radians(bpa - pa))
-r = np.cos(2*phi) * np.sin(2*dpa) / 2.
-#print(f'cross term ratio: {r:.2f}')
-xaxis = fitsdata.xaxis
-vaxis = fitsdata.vaxis
-dv = vaxis[1] - vaxis[0]
-dx = xaxis[1] - xaxis[0]
-res_off = fitsdata.res_off
-gbeam = np.exp2(-4 * (xaxis / res_off)**2)
-beampix = res_off / dx
-#print(f'res_off = {beampix:.1f} pixels')
-
-question = answer.copy()
-
+pa = 113
+dist = 139
 vsys = 4
-i0 = np.argmin(np.abs(xaxis + 70/139))
-i1 = np.argmin(np.abs(xaxis - 70/139)) + 1
-j0 = np.argmin(np.abs(vaxis - vsys + 6))
-j1 = np.argmin(np.abs(vaxis - vsys - 6)) + 1
-xaxis, vaxis = xaxis[i0:i1], vaxis[j0:j1] - vsys
-question, answer = question[j0:j1, i0:i1], answer[j0:j1, i0:i1]
+xmax = 70
+vmax = 6
+fitsfile = '/Users/yusukeaso/Desktop/IRAS16253_SBLB_12COh_robust_0.5.pvmajor.fits'
+
+fitsdata = Impvfits(fitsfile, pa=pa)
+question = np.squeeze(fitsdata.data)
+xaxis = fitsdata.xaxis * dist
+vaxis = fitsdata.vaxis - vsys
+res_off = fitsdata.res_off * dist
+
+i0 = np.argmin(np.abs(xaxis + xmax))
+i1 = np.argmin(np.abs(xaxis - xmax)) + 1
+j0 = np.argmin(np.abs(vaxis + vmax))
+j1 = np.argmin(np.abs(vaxis - vmax)) + 1
+xaxis, vaxis = xaxis[i0:i1], vaxis[j0:j1]
+question = question[j0:j1, i0:i1]
 gbeam = np.exp2(-4 * (xaxis / res_off)**2)
 beamarea = np.sum(gbeam)
 cmpthre = thre * rms / beamarea
-icent = np.argmax(gbeam)
 
 def clean(obs, threshold, gain=0.01):
     residual = obs.copy()
@@ -59,28 +52,40 @@ def clean(obs, threshold, gain=0.01):
     #       * (np.roll(clncmp, -1) < cmpthre)] = 0
     return clncmp, residual
 
-#print(len(xaxis), 'pixels')
-deconv, clnres = [], []
-for v, y, p in zip(vaxis, question, answer):
-    #print(f'{v:.3f} km/s')
-    clncmp, residual = clean(y, threshold=1 * rms)
+deconv = []
+for v, y in zip(vaxis, question):
+    clncmp, _ = clean(y, threshold=1 * rms)
     deconv.append(clncmp)
-    clnres.append(residual)
 deconv = np.array(deconv)
 conv = np.array([np.convolve(d, gbeam, mode='same') for d in deconv])
+deconv_pad = np.squeeze(fitsdata.data) * 0
+deconv_pad[j0:j1, i0:i1] = deconv
+###########################################################
 
-xaxis = xaxis / res_off
+w = wcs.WCS(naxis=2)
+hdu = fits.PrimaryHDU(deconv_pad, header=w.to_header())
+h = fits.open(fitsfile)[0].header
+for k in h.keys():
+    if not ('COMMENT' in k or 'HISTORY' in k):
+        hdu.header[k] = h[k]
+hdu.header['BUNIT'] = 'Jy/pixel'
+hdu = fits.HDUList([hdu])
+hdu.writeto('clean.fits', overwrite=True)
+
+###########################################################
 levels = np.arange(1, 10) * 6 * rms
 levels = np.sort(np.r_[-levels, levels])
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 c = ax.pcolormesh(xaxis, vaxis, question, shading='nearest',
-                  cmap='jet', vmin=0 * rms)
+                  cmap='viridis', vmin=0)
 fig.colorbar(c, ax=ax, label='Jy/beam')
 ax.contour(xaxis, vaxis, question, colors='w',
            levels=levels, linewidths=1)
-ax.set_xlabel('Position (beam)')
+ax.set_xlabel('Position (au)')
 ax.set_ylabel('Velocity (km/s)')
+ax.set_xlim(-xmax, xmax)
+ax.set_ylim(-vmax, vmax)
 fig.savefig('q_clean.png')
 #plt.show()
 
@@ -89,12 +94,14 @@ levels = np.sort(np.r_[-levels, levels])
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 c = ax.pcolormesh(xaxis, vaxis, conv, shading='nearest',
-                  cmap='jet', vmin=0 * rms)
+                  cmap='viridis', vmin=0)
 fig.colorbar(c, ax=ax, label='Jy/beam')
 ax.contour(xaxis, vaxis, conv, colors='w',
            levels=levels, linewidths=1)
-ax.set_xlabel('Position (beam)')
+ax.set_xlabel('Position (au)')
 ax.set_ylabel('Velocity (km/s)')
+ax.set_xlim(-xmax, xmax)
+ax.set_ylim(-vmax, vmax)
 fig.savefig('c_clean.png')
 #plt.show()
 
@@ -103,82 +110,29 @@ levels = np.sort(np.r_[-levels, levels])
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 c = ax.pcolormesh(xaxis, vaxis, (question - conv) / rms, shading='nearest',
-                  cmap='jet', vmin=-3, vmax=3)
+                  cmap='viridis', vmin=-3, vmax=3)
 fig.colorbar(c, ax=ax, label='sigma')
 ax.contour(xaxis, vaxis, question - conv, colors='w',
            levels=levels, linewidths=1)
-ax.set_xlabel('Position (beam)')
+ax.set_xlabel('Position (au)')
 ax.set_ylabel('Velocity (km/s)')
+ax.set_xlim(-xmax, xmax)
+ax.set_ylim(-vmax, vmax)
 fig.savefig('qvsc_clean.png')
 #plt.show()
 
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
-c = ax.pcolormesh(xaxis, vaxis, answer, shading='nearest',
-                  cmap='jet', vmin=0, vmax=0.68)
-fig.colorbar(c, ax=ax, label='Jy/pixel')
-levels = np.arange(1, 10) * 3 * cmpthre
-levels = np.sort(np.r_[-levels, levels])
-ax.contour(xaxis, vaxis, answer, colors='w',
-           levels=levels, linewidths=1)
-ax.set_xlabel('Position (beam)')
-ax.set_ylabel('Velocity (km/s)')
-fig.savefig('a_clean.png')
-#plt.show()
-
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
 c = ax.pcolormesh(xaxis, vaxis, deconv, shading='nearest',
-                  cmap='jet', vmin=0)
+                  cmap='viridis', vmin=0)
 fig.colorbar(c, ax=ax, label='Jy/pixel')
 levels = np.arange(1, 10) * 3 * cmpthre
 levels = np.sort(np.r_[-levels, levels])
 ax.contour(xaxis, vaxis, deconv, colors='w',
            levels=levels, linewidths=1)
-ax.set_xlabel('Position (beam)')
+ax.set_xlabel('Position (au)')
 ax.set_ylabel('Velocity (km/s)')
+ax.set_xlim(-xmax, xmax)
+ax.set_ylim(-vmax, vmax)
 fig.savefig('d_clean.png')
 #plt.show()
-
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
-c = ax.pcolormesh(xaxis, vaxis, answer - deconv, shading='nearest',
-                  cmap='jet', vmin=-0.68, vmax=0.68)
-fig.colorbar(c, ax=ax, label='Jy/pixel')
-levels = 2**np.arange(10) * cmpthre
-levels = np.sort(np.r_[-levels, levels])
-ax.contour(xaxis, vaxis, answer - deconv, colors='w',
-           levels=levels, linewidths=1)
-ax.plot(vaxis * 0 - 1, vaxis, '--w')
-ax.set_xlabel('Position (beam)')
-ax.set_ylabel('Velocity (km/s)')
-fig.savefig('avsd_clean.png')
-#plt.show()
-
-xnew = np.linspace(xaxis[0], xaxis[-1], (len(xaxis) - 1) * 10 + 1)
-result = []
-for a, d, c, v in zip(answer, deconv, question, vaxis):
-    wa = wd = wc = 0
-    wa = xaxis[(a > 0) * (xaxis < 1.)]
-    wa = np.nan if len(wa) == 0 else wa[-1]
-    d[(np.roll(d, 1) == 0) * (np.roll(d, -1) == 0)] == 0
-    ynew = interp1d(xaxis, d, kind='linear')(xnew)
-    g = ynew[:-1] - ynew[1:]
-    g[xnew[:-1] > 1.0] = 0
-    #wd = xnew[np.argmax(g)]
-    wd = xnew[(ynew > cmpthre) * (xnew < 1.)]
-    wd = np.nan if len(wd) == 0 else wd[-1]
-    ynew = interp1d(xaxis, c, kind='cubic')(xnew)
-    wc = xnew[(ynew > rms * thre) * (xnew < 1.)]
-    wc = np.nan if len(wc) == 0 else wc[-1]
-    w = 3**(1 - (np.abs(v / dv) - 0.5) / 4.)
-    w = 0 if w < 0.32 else w
-    if w > 0:
-        r = (wc - wd) / w
-        result.append([int(round(1 / rms)), w, wc, wd])
-result = np.array(result)
-result = np.concatenate((result[:9], result[-1:-10:-1]), axis=0)
-with open('/Users/yusukeaso/Desktop/rec_clean.dat', 'a') as f:
-    np.savetxt(f, result)
-#for r in result:
-#    print(f'{r[0]:.2f} {r[1]:.2f} {r[2]:.2f}')
