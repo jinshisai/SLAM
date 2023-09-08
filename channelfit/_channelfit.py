38ed9e9aa45cf8d2f5d538c4c9ef6906ef334f45
Yusuke Aso <98569855+yusukeaso-astron@users.noreply.github.com>
2023-09-09 00:38:19 +0900

    self.Rnest

--- channelfit/_channelfit.py
+++ channelfit/_channelfit.py
@@ -233,9 +233,9 @@ class ChannelFit():
 
         
     def get_vlos(self, Rc: float) -> np.ndarray:
-        vkep = self.r**(-1/2)
+        vkep = self.Rnest**(-1/2)
         vjrot = np.sqrt(Rc) / self.r
-        vr = -self.r**(-1/2) * np.sqrt(2 - Rc / self.r)
+        vr = -self.Rnest**(-1/2) * np.sqrt(2 - Rc / self.Rnest)
         vr[self.Rnest < Rc] = 0
         vrot = np.where(self.Rnest < Rc, vkep, vjrot)
         vlos = vrot * self.erot + vr * self.erad
