269f1bf1112120dcd9bc4e1658e9eb4dcb2a9a89
Yusuke Aso <98569855+yusukeaso-astron@users.noreply.github.com>
2023-09-29 00:16:53 +0900

    small gauss area

--- channelfit/_channelfit.py
+++ channelfit/_channelfit.py
@@ -227,8 +227,9 @@ class ChannelFit():
                   + f' {npixnest:d}')
         print('-----------------------------')
         
-        xb = (np.arange(npixnest + 1) - npixnest // 2) * dpix
-        yb = (np.arange(npixnest + 1) - npixnest // 2) * dpix
+        ngauss = int((self.bmaj / dpix + 0.5) * 1.5)
+        xb = (np.arange(2 * ngauss + 1) - ngauss) * dpix
+        yb = (np.arange(2 * ngauss + 1) - ngauss) * dpix
         bpa_on_disk = self.bpa - pa
         xb, yb = rot(*np.meshgrid(xb, yb), np.radians(bpa_on_disk))
         gaussbeam = np.exp(-((yb / self.bmaj)**2 + (xb / self.bmin)**2))
