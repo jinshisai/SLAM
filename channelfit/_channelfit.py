ceafa611940fa39bb70302e38d819554daeff40b
Yusuke Aso <98569855+yusukeaso-astron@users.noreply.github.com>
2023-11-17 17:31:34 +0900

    minor

--- channelfit/_channelfit.py
+++ channelfit/_channelfit.py
@@ -339,14 +339,14 @@ class ChannelFit():
         i, j = np.meshgrid(n, n)
         par0 = np.ravel(d[i, j] / gsum)
         def model(x, *par):
-            xin, yin = x
+            xin, yin = np.reshape(x, (2, n, n))
             f = np.reshape(par, (n, n))
             f = RGI((ymodel, xmodel), f, method='linear',
                     bounds_error=False, fill_value=0)
             f = convolve(f((yin, xin)), g, mode='same')
             return np.ravel(f)
         bounds = [np.zeros(n * n), par0.clip(self.sigma / gsum, None) * 10]
-        xdata = np.array([Xi, Yi])
+        xdata = np.ravel([Xi, Yi])
         popt, _ = curve_fit(model, xdata, np.ravel(d), bounds, x0=par0)
         f = RGI((ymodel, xmodel), np.reshape(popt, (n, n)), method='linear',
                 bounds_error=False, fill_value=0)
