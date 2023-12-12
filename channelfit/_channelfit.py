9560c631df3a653cf537a5becefa3e33f366eb1a
Yusuke Aso <98569855+yusukeaso-astron@users.noreply.github.com>
2023-12-13 00:46:41 +0900

    fftshift

--- channelfit/_channelfit.py
+++ channelfit/_channelfit.py
@@ -154,11 +154,11 @@ def fftdeconvolve(data: np.ndarray, x: np.ndarray, y: np.ndarray,
     v = np.fft.fftshift(np.fft.fftfreq(ny, d=dy))
     u, v = np.meshgrid(u, v)
     phase0 = 2 * np.pi * (u * (xg[-1] + dx) + v * (yg[-1] + dy))
-    FTg = np.fft.fft2(g) * np.exp(-1j * phase0)
+    FTg = np.fft.fftshift(np.fft.fft2(g)) * np.exp(-1j * phase0)
     phase0 = 2 * np.pi * (u * (xd[-1] + dx) + v * (yd[-1] + dy))
-    FTd = np.fft.fft2(d) * np.exp(-1j * phase0)
+    FTd = np.fft.fftshift(np.fft.fft2(d)) * np.exp(-1j * phase0)
     FTdnew = FTd / FTg * np.exp(1j * phase0)
-    dnew = np.real(np.fft.ifft2(FTdnew))
+    dnew = np.real(np.fft.ifft2(np.fft.ifftshift(FTdnew)))
     if len(x) % 2 == 0:
         dnew = np.concatenate((np.zeros((np.shape(dnew)[0], 1)), dnew), axis=1)
     if len(y) % 2 == 0:
