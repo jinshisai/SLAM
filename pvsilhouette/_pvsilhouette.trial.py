1b9e9be8341b112c4df5921d6965ab8b7615234e
Yusuke Aso <98569855+yusukeaso-astron@users.noreply.github.com>
2023-12-20 22:14:18 +0900

    org

--- pvsilhouette/_pvsilhouette.trial.py
+++ pvsilhouette/_pvsilhouette.trial.py
@@ -14,8 +14,9 @@ Note. FITS files with multiple beams are not supported. The dynamic range for xl
 import numpy as np
 import matplotlib.pyplot as plt
 from astropy.io import fits
-from astropy import constants
+from astropy import constants, units
 from astropy.coordinates import SkyCoord
+from scipy.interpolate import interp1d
 from scipy.interpolate import RegularGridInterpolator as RGI
 from tqdm import tqdm
 import warnings
@@ -32,9 +33,12 @@ class PVSilhouette():
 
     def read_cubefits(self, cubefits: str, center: str = None,
                       dist: float = 1, vsys: float = 0,
-                      xmax: float = 1e4, ymax: float = 1e4,
-                      vmin: float = -100, vmax: float = 100,
-                      sigma: float = None) -> dict:
+                      xmin: float = None, xmax: float = None,
+                      ymin: float = None, ymax: float = None,
+                      vmin: float = None, vmax: float = None,
+                      xskip: int = 1, yskip: int = 1,
+                      sigma: float = None,
+                      centering_velocity: bool = False) -> dict:
         """
         Read a position-velocity diagram in the FITS format.
 
@@ -48,16 +52,22 @@ class PVSilhouette():
             Distance of the target, used to convert arcsec to au.
         vsys : float
             Systemic velocity of the target.
-        xmax : float
-            The R.A. axis is limited to (-xmax, xmax) in the unit of au.
-        ymax : float
-            The Dec. axis is limited to (-xmax, xmax) in the unit of au.
+        xmin, xmax : float
+            The R.A. axis is limited to (xmin, xmax) in the unit of au.
+        ymin, ymax : float
+            The Dec. axis is limited to (ymin, ymax) in the unit of au.
         vmax : float
             The velocity axis of the PV diagram is limited to (vmin, vmax).
         vmin : float
             The velocity axis of the PV diagram is limited to (vmin, vmax).
+        xskip : int
+            Skip xskip pixels in the x axis.
+        yskip : int
+            Skip yskip pixels in the y axis.
         sigma : float
             Standard deviation of the FITS data. None means automatic.
+        centering_velocity : bool
+            One channel has the exact velocity of vsys by interpolation.
 
         Returns
         ----------
@@ -70,24 +80,47 @@ class PVSilhouette():
         if center is None:
             cx, cy = 0, 0
         else:
-            coord = SkyCoord(center, frame='icrs')
-            cx = coord.ra.degree - h['CRVAL1']
-            cy = coord.dec.degree - h['CRVAL2']
+            c0 = SkyCoord('00h00m00s 00d00m00s', frame='icrs')
+            c1 = [h['CRVAL1'] * units.degree, h['CRVAL2'] * units.degree]
+            c1 = c0.spherical_offsets_by(*c1)
+            c3 = c1.spherical_offsets_to(SkyCoord(center, frame='icrs'))
+            cx = c3[0].degree
+            cy = c3[1].degree
         if sigma is None:
             sigma = np.mean([np.nanstd(d[:2]), np.std(d[-2:])])
             print(f'sigma = {sigma:.3e}')
         x = (np.arange(h['NAXIS1']) - h['CRPIX1'] + 1) * h['CDELT1']
         y = (np.arange(h['NAXIS2']) - h['CRPIX2'] + 1) * h['CDELT2']
         v = (np.arange(h['NAXIS3']) - h['CRPIX3'] + 1) * h['CDELT3']
+        crpix = int(h['CRPIX1']) - 1
+        startpix = crpix % xskip
+        x = x[startpix::xskip]
+        h['CRPIX1'] = (crpix - startpix) // xskip + 1
+        h['CDELT1'] = h['CDELT1'] * xskip
+        d = d[:, :, startpix::xskip]
+        crpix = int(h['CRPIX2']) - 1
+        startpix = crpix % yskip
+        y = y[startpix::yskip]
+        h['CRPIX2'] = (crpix - startpix) // yskip + 1
+        h['CDELT2'] = h['CDELT2'] * yskip
+        d = d[:, startpix::yskip, :]
         v = v + h['CRVAL3']
         x = (x - cx) * 3600. * dist  # au
         y = (y - cy) * 3600. * dist  # au
         v = (1. - v / h['RESTFRQ']) * cc / 1.e3 - vsys  # km/s
-        i0, i1 = np.argmin(np.abs(x - xmax)), np.argmin(np.abs(x + xmax))
-        j0, j1 = np.argmin(np.abs(y + ymax)), np.argmin(np.abs(y - ymax))
-        k0, k1 = np.argmin(np.abs(v - vmin)), np.argmin(np.abs(v - vmax))
+        i0 = 0 if xmax is None else np.argmin(np.abs(x - xmax))
+        i1 = len(x) if xmin is None else np.argmin(np.abs(x - xmin))
+        j0 = 0 if ymin is None else np.argmin(np.abs(y - ymin))
+        j1 = len(y) if ymax is None else np.argmin(np.abs(y - ymax))
+        x, y = x[i0:i1 + 1], y[j0:j1 + 1]
+        if centering_velocity:
+            f = interp1d(v, d, kind='cubic', bounds_error=False,
+                         fill_value=0, axis=0)
+            d = f(v := v - v[np.argmin(np.abs(v))])
+        k0 = 0 if vmin is None else np.argmin(np.abs(v - vmin))
+        k1 = len(v) if vmax is None else np.argmin(np.abs(v - vmax))
         self.offpix = (i0, j0, k0)
-        x, y, v = x[i0:i1 + 1], y[j0:j1 + 1], v[k0:k1 + 1],
+        v = v[k0:k1 + 1]
         d =  d[k0:k1 + 1, j0:j1 + 1, i0:i1 + 1]
         dx, dy, dv = x[1] - x[0], y[1] - y[0], v[1] - v[0]
         if 'BMAJ' in h.keys():
@@ -97,9 +130,9 @@ class PVSilhouette():
         else:
             bmaj, bmin, bpa = dy, -dx, 0
             print('No valid beam in the FITS file.')
-        self.x, self.dx = x, dx
-        self.y, self.dy = y, dy
-        self.v, self.dv = v, dv
+        self.x, self.dx, self.nx = x, dx, len(x)
+        self.y, self.dy, self.ny = y, dy, len(y)
+        self.v, self.dv, self.nv = v, dv, len(v)
         self.data, self.header, self.sigma = d, h, sigma
         self.bmaj, self.bmin, self.bpa = bmaj, bmin, bpa
         self.cubefits, self.dist, self.vsys = cubefits, dist, vsys
@@ -170,7 +203,8 @@ class PVSilhouette():
                sigma: float = None):
         if not (cubefits is None):
             self.read_cubefits(cubefits, center, dist, vsys,
-                               xmax, xmax, vmin, vmax, sigma)
+                               -xmax, xmax, -xmax, xmax, vmin, vmax,
+                               1, 1, sigma)
         x, y, v = self.x, self.y, self.v
         sigma, d = self.sigma, self.data
         n = np.floor(xmax / self.dy)
@@ -206,39 +240,41 @@ class PVSilhouette():
                 figname: str = 'PVsilhouette',
                 show: bool = False,
                 progressbar: bool = True,
-                kwargs_emcee_corner: dict = {}):
-        majobs = np.where(self.dpvmajor > cutoff * self.sigma, 1, 0)
-        minobs = np.where(self.dpvminor > cutoff * self.sigma, 1, 0)
-        x, v = np.meshgrid(self.x, self.v)
-        def minmax(a: np.ndarray, b: np.ndarray, s: str, m: np.ndarray):
-            rng = a[(b >= 0 if s == '+' else b < 0) * (m > 0.5)]
-            if len(rng) == 0:
-                return 0, 0
-            else:
-                return np.min(rng), np.max(rng)
-        rng = np.array([[[minmax(a, b, s, m)
-                          for m in [majobs, minobs]]
-                         for s in ['-', '+']]
-                        for a, b in zip([x, v], [v, x])])
-        def combine(r: np.ndarray):
-            return np.min(r[:, 0]), np.max(r[:, 1])
-        rng = [[combine(r) for r in rr] for rr in rng]
-        mask = [[(s * a > 0) * ((b < r[0]) + (r[1] < b))
-                 for s, r in zip([-1, 1], rr)]
-                for a, b, rr in zip([v, x], [x, v], rng)]
-        mask = np.sum(mask, axis=(0, 1)) + (vmask[0] < v) * (v < vmask[1])
-        majobs = np.where(mask, np.nan, majobs)
-        minobs = np.where(mask, np.nan, minobs)
-        majsig = 1
-        minsig = 1
-        def calcchi2(majmod: np.ndarray, minmod: np.ndarray):
-            chi2 =   np.nansum((majobs - majmod)**2 / majsig**2) \
-                   + np.nansum((minobs - minmod)**2 / minsig**2)
-            return chi2
-        
-        chi2max1 = calcchi2(np.ones_like(majobs), np.ones_like(minobs))
-        chi2max0 = calcchi2(np.zeros_like(majobs), np.zeros_like(minobs))
-        chi2max = np.min([chi2max0, chi2max1])
+                kwargs_emcee_corner: dict = {},
+                minabserr: float = 0.1,
+                minrelerr: float = 0.01):
+        Nyquistskip = int(np.round(self.bmaj / self.dx / 2))
+        x = self.x[::Nyquistskip]
+        vintp = np.linspace(self.v[0], self.v[-1], (len(self.v) - 1) * 10 + 1)
+        dvintp = vintp[1] - vintp[0]
+        majintp = interp1d(self.v, self.dpvmajor, kind='cubic', axis=0)(vintp)
+        minintp = interp1d(self.v, self.dpvminor, kind='cubic', axis=0)(vintp)
+        vobs = []
+        vobserr = []
+        for d in [majintp.T[::Nyquistskip, :], minintp.T[::Nyquistskip, :]]:
+            vtmp = []
+            dvtmp = []
+            for c in d:
+                grad = (np.roll(c, -1) - np.roll(c, 1)) / (2 * dvintp)
+                cond = (c > cutoff * self.sigma)
+                cond = cond * ((vintp < vmask[0]) + (vmask[1] < vintp))
+                grad, vgood = grad[cond], vintp[cond]
+                err = self.sigma / np.abs(grad)
+                if len(vgood) == 0:
+                    vtmp.append([np.nan, np.nan])
+                    dvtmp.append([np.nan, np.nan])
+                else:
+                    vmin = vgood[0] if vgood[0] <= 0 else np.nan
+                    vmax = vgood[-1] if vgood[-1] >= 0 else np.nan
+                    vtmp.append([vmin, vmax])
+                    dvtmp.append([err[0], err[-1]])
+            vobs.append(vtmp)
+            vobserr.append(dvtmp)
+        vobs = np.moveaxis(vobs, 1, 2)
+        vobserr = np.moveaxis(vobserr, 1, 2)
+        aerr = np.full(np.shape(vobserr), minabserr * self.dv)
+        rerr = minrelerr * vobs
+        vobserr = np.max([vobserr, aerr, rerr], axis=0)
         def getquad(m):
             nv, nx = np.shape(m)
             q =   np.sum(m[:nv//2, :nx//2]) + np.sum(m[nv//2:, nx//2:]) \
@@ -246,21 +282,13 @@ class PVSilhouette():
             return int(np.sign(q))
         majquad = getquad(self.dpvmajor)
         minquad = getquad(self.dpvminor) * (-1)
-        def makemodel(Mstar, Rc, alphainfall, outputvel=False):
-            a = velmax(self.x, Mstar=Mstar, Rc=Rc,
+        def makemodel(Mstar, Rc, alphainfall):
+            a = velmax(x, Mstar=Mstar, Rc=Rc,
                        alphainfall=alphainfall, incl=incl)
-            major = []
-            for min, max in zip(a['major']['vlosmin'], a['major']['vlosmax']):
-                major.append(np.where((min < self.v) * (self.v < max), 1, 0))
-            major = np.transpose(major)[:, ::majquad]
-            minor = []
-            for min, max in zip(a['minor']['vlosmin'], a['minor']['vlosmax']):
-                minor.append(np.where((min < self.v) * (self.v < max), 1, 0))
-            minor = np.transpose(minor)[:, ::minquad]
-            if outputvel:
-                return major, minor, a
-            else:
-                return major, minor
+            vmod = [[a[i][j][::k] for j in ['vlosmin', 'vlosmax']]
+                    for i, k in zip(['major', 'minor'], [majquad, minquad])]
+            return np.array(vmod)
+
         p_fixed = np.array([Mstar_fixed, Rc_fixed, alphainfall_fixed])
         if None in p_fixed:
             labels = np.array(['log Mstar', 'log Rc', r'log $\alpha$'])
@@ -279,13 +307,11 @@ class PVSilhouette():
                     bar.update(1)
                 q = p_fixed.copy()
                 q[p_fixed == None] = 10**p
-                chi2 = calcchi2(*makemodel(*q))
-                return -np.inf if chi2 > chi2max else -0.5 * chi2
+                chi2 = np.nansum(((vobs - makemodel(*q)) / vobserr)**2)
+                return -0.5 * chi2
             plim = np.array([Mstar_range, Rc_range, alphainfall_range])
             plim = np.log10(plim[p_fixed == None]).T
             mcmc = emcee_corner(plim, lnprob, simpleoutput=False, **kwargs)
-            if np.isinf(lnprob(mcmc[0])):
-                print('No model is better than the all-0 or all-1 models.')
             popt = p_fixed.copy()
             popt[p_fixed == None] = 10**mcmc[0]
             plow = p_fixed.copy()
@@ -303,31 +329,32 @@ class PVSilhouette():
         print(f'Rc = {plow[1]:.0f}, {popt[1]:.0f}, {phigh[1]:.0f} au')
         print(f'alpha = {plow[2]:.2f}, {popt[2]:.2f}, {phigh[2]:.2f}')
 
-        majmod, minmod, a = makemodel(*popt, outputvel=True)
+        a = velmax(x, Mstar=popt[0], Rc=popt[1],
+                   alphainfall=popt[2], incl=incl)
         fig = plt.figure()
         ax = fig.add_subplot(1, 2, 1)
-        z = np.where(mask, -(mask.astype('int')), (majobs - majmod)**2)
-        ax.pcolormesh(self.x, self.v, z, cmap='bwr', vmin=-1, vmax=1, alpha=0.1)
         ax.contour(self.x, self.v, self.dpvmajor,
                    levels=np.arange(1, 10) * 3 * self.sigma, colors='k')
-        ax.plot(self.x * majquad, a['major']['vlosmax'], '-r')
-        ax.plot(self.x * majquad, a['major']['vlosmin'], '-r')
+        ax.plot(x * majquad, a['major']['vlosmax'], '-r')
+        ax.plot(x * majquad, a['major']['vlosmin'], '-r')
+        ax.errorbar(x, vobs[0][0], yerr=vobserr[0][0], fmt='ob', ms=2)
+        ax.errorbar(x, vobs[0][1], yerr=vobserr[0][1], fmt='ob', ms=2)
         ax.set_xlabel('major offset (au)')
         ax.set_ylabel(r'$V-V_{\rm sys}$ (km s$^{-1}$)')
         ax.set_ylim(np.min(self.v), np.max(self.v))
         ax = fig.add_subplot(1, 2, 2)
-        z = np.where(mask, -(mask.astype('int')), (minobs - minmod)**2)
-        ax.pcolormesh(self.x, self.v, z, cmap='bwr', vmin=-1, vmax=1, alpha=0.1)
         ax.contour(self.x, self.v, self.dpvminor,
                    levels=np.arange(1, 10) * 3 * self.sigma, colors='k')
-        ax.plot(self.x * minquad, a['minor']['vlosmax'], '-r')
-        ax.plot(self.x * minquad, a['minor']['vlosmin'], '-r')
+        ax.plot(x * minquad, a['minor']['vlosmax'], '-r')
+        ax.plot(x * minquad, a['minor']['vlosmin'], '-r')
+        ax.errorbar(x, vobs[1][0], yerr=vobserr[1][0], fmt='ob', ms=2)
+        ax.errorbar(x, vobs[1][1], yerr=vobserr[1][1], fmt='ob', ms=2)
         ax.set_xlabel('minor offset (au)')
         ax.set_ylim(self.v.min(), self.v.max())
         ax.set_title(r'$M_{*}$'+f'={popt[0]:.2f}'+r'$M_{\odot}$'
             +', '+r'$R_{c}$'+f'={popt[1]:.0f} au'
             +'\n'+r'$\alpha$'+f'={popt[2]:.2f}'
-            +', '+r'$\alpha ^{2} M_{*}$'+f'={popt[0] * popt[2]**2:.2}')
+            +', '+r'$\alpha ^{2} M_{*}$'+f'={popt[0] * popt[2]**2:.2f}')
         fig.tight_layout()
         fig.savefig(figname + '.model.png')
         if show: plt.show()
