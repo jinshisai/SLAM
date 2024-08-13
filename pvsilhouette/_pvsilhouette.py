75a9cbc46d698cfec68b4c8437f00b145627695c
jinshisai <jn.insa.sai@gmail.com>
2024-08-13 14:31:50 +0800

    New methods are added.

--- pvsilhouette/_pvsilhouette.py
+++ pvsilhouette/_pvsilhouette.py
@@ -376,6 +376,9 @@ class PVSilhouette():
                 log_ftau_range: list = [-1., 3.],
                 log_frho_range: list = [-1., 4.],
                 sig_mdl_range: list = [0., 10.],
+                fixed_params: dict = {'Mstar': None, 
+                'Rc': None, 'alphainfall': None, 'fflux': None,
+                'log_ftau': None, 'log_frho': None, 'sig_mdl': None},
                 Mstar_fixed: float = None,
                 Rc_fixed: float = None,
                 alphainfall_fixed: float = None,
@@ -384,6 +387,7 @@ class PVSilhouette():
                 log_frho_fixed: float = None,
                 sig_mdl_fixed: float = 0.,
                 vmask: list = [0, 0],
+                filename: str = 'PVsilhouette',
                 figname: str = 'PVsilhouette',
                 show: bool = False,
                 progressbar: bool = True,
@@ -393,17 +397,17 @@ class PVSilhouette():
                 beam = None, linewidth = None,
                 p0 = None,
                 nsubgrid = 1, n_nest = [3, 3], reslim = 5):
-        # Observed pv diagrams
+        # Observed PV diagrams
         majobs = self.dpvmajor.copy()
         minobs = self.dpvminor.copy()
         # correction factor for over sampling
         beam_area = np.pi/(4.*np.log(2.)) * self.bmaj * self.bmin # beam area
         Rarea = beam_area / self.dx / self.dx # area ratio
-        #print('Rarea %.2f'%Rarea)
 
 
         # grid & mask
         x, v = np.meshgrid(self.x, self.v)
+        '''
         def minmax(a: np.ndarray, b: np.ndarray, s: str, m: np.ndarray):
             rng = a[(b >= 0 if s == '+' else b < 0) * (m > 0.5)]
             if len(rng) == 0:
@@ -420,7 +424,8 @@ class PVSilhouette():
         mask = [[(s * a > 0) * ((b < r[0]) + (r[1] < b))
                  for s, r in zip([-1, 1], rr)]
                 for a, b, rr in zip([v, x], [x, v], rng)]
-        mask = (vmask[0] < v) * (v < vmask[1]) # np.sum(mask, axis=(0, 1)) 
+        '''
+        mask = (vmask[0] < v) * (v < vmask[1])
         majobs = np.where(mask, np.nan, majobs)
         minobs = np.where(mask, np.nan, minobs)
 
@@ -431,17 +436,9 @@ class PVSilhouette():
             chi2 = np.nansum((majobs - majmod)**2 / majsig**2) \
                    + np.nansum((minobs - minmod)**2 / minsig**2)
             return chi2 / np.sqrt(Rarea) # correct over sampling
-        #chi2max1 = calcchi2(majobs, minobs)
-        #chi2max0 = calcchi2(majobs, minobs)
-        #chi2max = np.min([chi2max0, chi2max1])
 
 
         # get quadrant
-        def getquad(m):
-            nv, nx = np.shape(m)
-            q =   np.sum(m[:nv//2, :nx//2]) + np.sum(m[nv//2:, nx//2:]) \
-                - np.sum(m[nv//2:, :nx//2]) - np.sum(m[:nv//2, nx//2:])
-            return int(np.sign(q))
         majquad = getquad(self.dpvmajor) if signmajor is None else signmajor
         minquad = getquad(self.dpvminor) * (-1) if signminor is None else signminor
         obsmax = max(np.nanmax(majobs), np.nanmax(minobs))
@@ -451,27 +448,13 @@ class PVSilhouette():
         mpvd = MockPVD(self.x, self.x, self.v, 
             nsubgrid = nsubgrid, nnest = n_nest, 
             beam = self.beam, reslim = reslim)
+        rout = np.nanmax(self.x)
         def makemodel(Mstar, Rc, alphainfall, fflux, log_ftau, log_frho):
-            # f_tau is in log scale
-            '''
-            major = mockpvd(self.x, self.x, self.v, Mstar, Rc,
-                alphainfall=alphainfall, incl=incl, axis='major', withkepler=True,
-                rho_jump = 10.**log_frho, tauscale = 10.**log_ftau,
-                fscale = obsmax * fflux, beam = self.beam, pa=pa_maj, 
-                rout=np.nanmax(self.x), linewidth = linewidth,
-                nsubgrid = nsubgrid)
-            minor = mockpvd(self.x, self.x, self.v, Mstar, Rc,
-                alphainfall=alphainfall, incl=incl, axis='minor', withkepler=True,
-                rho_jump = 10.**log_frho, tauscale = 10.**log_ftau,
-                fscale = obsmax * fflux, beam = self.beam, pa=pa_min, 
-                rout=np.nanmax(self.x), linewidth = linewidth,
-                nsubgrid = nsubgrid)
-            '''
             major, minor = mpvd.generate_mockpvd(
                 Mstar, Rc, alphainfall, 
                 fflux = fflux * obsmax, frho = 10.**log_frho, ftau = 10.**log_ftau,
                 incl = incl, withkepler = True, linewidth = linewidth,
-                rout = np.nanmax(self.x), pa = [pa_maj, pa_min],
+                rout = rout, pa = [pa_maj, pa_min],
                 axis = 'both')
             # quadrant
             major = major[:, ::majquad] #[:,step//2::step]
@@ -480,40 +463,34 @@ class PVSilhouette():
 
 
         # Fitting
-        p_fixed = np.array([Mstar_fixed, Rc_fixed, alphainfall_fixed, 
-            fflux_fixed, log_ftau_fixed, log_frho_fixed, sig_mdl_fixed])
+        _fixed_params = {'Mstar': None, 
+        'Rc': None, 'alphainfall': None, 'fflux': None,
+        'log_ftau': None, 'log_frho': None, 'sig_mdl': None}
+        _fixed_params.update(fixed_params)
+        p_fixed = np.array(list(_fixed_params.values()))
+        #p_fixed = np.array([Mstar_fixed, Rc_fixed, alphainfall_fixed, 
+        #    fflux_fixed, log_ftau_fixed, log_frho_fixed, sig_mdl_fixed])
         if None in p_fixed:
             labels = np.array(['Mstar', 'Rc', r'$\alpha$', r'$f_\mathrm{flux}$', 
                 r'log $f_\tau$', r'log $f_\rho$', r'$\sigma_\mathrm{model}$'])
             labels = labels[p_fixed == None]
-            kwargs0 = {'nwalkers_per_ndim':2, 'nburnin':2000, 'nsteps':1000, # 16, 100, 500
+            kwargs0 = {'nwalkers_per_ndim':4, 'nburnin':500, 'nsteps':500,
                        'rangelevel': None, 'labels':labels,
                        'figname':figname+'.corner.png', 'show_corner':show,
-                       #'moves': [(emcee.moves.DEMove(), 0.8),(emcee.moves.DESnookerMove(), 0.2),]
                        }
             kwargs = dict(kwargs0, **kwargs_emcee_corner)
             # progress bar
             if progressbar:
                 total = kwargs['nwalkers_per_ndim'] * len(p_fixed[p_fixed == None])
-                total *= kwargs['nburnin'] + kwargs['nsteps'] #+ 2
+                total *= kwargs['nburnin'] + kwargs['nsteps']
                 bar = tqdm(total=total)
                 bar.set_description('Within the ranges')
-            # log likelihood
-            #def lnprob(p):
-            #    if progressbar:
-            #        bar.update(1)
-            #    q = p_fixed.copy()
-            #    #q[p_fixed == None] = 10**p
-            #    q[p_fixed == None] = p # in linear scale
-            #    chi2 = calcchi2(*makemodel(*q[:-1]), q[-1])
-            #    return -0.5 * chi2 # -np.inf if chi2 > chi2max else
             # Modified log likelihood
             def lnprob(p):
                 if progressbar:
                     bar.update(1)
                 # parameter
                 q = p_fixed.copy()
-                #q[p_fixed == None] = 10**p
                 q[p_fixed == None] = p # in linear scale
                 # updated sigma
                 majsig2 = majsig**2. + q[-1]**2.
@@ -526,13 +503,14 @@ class PVSilhouette():
             sig_mdl_range = np.array(sig_mdl_range) * self.sigma
             plim = np.array([Mstar_range, Rc_range, alphainfall_range, 
                 fflux_range, log_ftau_range, log_frho_range, list(sig_mdl_range)])
-            #plim = np.log10(plim[p_fixed == None]).T
             plim = plim[p_fixed == None].T
+
+            # run mcmc fitting
             mcmc = emcee_corner(plim, lnprob, simpleoutput=False, **kwargs)
-            if np.isinf(lnprob(mcmc[0])):
-                print('No model is better than the all-0 or all-1 models.')
+
+            # best parameters & errors
             popt = p_fixed.copy()
-            popt[p_fixed == None] = mcmc[0] # 10**
+            popt[p_fixed == None] = mcmc[0]
             plow = p_fixed.copy()
             plow[p_fixed == None] = mcmc[1]
             pmid = p_fixed.copy()
@@ -556,49 +534,204 @@ class PVSilhouette():
         print(f'f_rho = {plow[5]:.2f}, {popt[5]:.2f}, {phigh[5]:.2f}')
 
 
-        # plot
-        majmod, minmod = makemodel(*popt[:-1])
-        majres = np.where(mask, -(mask.astype('int')), (majobs - majmod) / majsig)
-        minres = np.where(mask, -(mask.astype('int')), (minobs - minmod) / minsig)
+        # write out result
+        self.writeout_fitres(filename + '.popt')
 
-        for zmin, zmaj, vmin, vmax, cmap, alpha, lbl in zip(
-            [minres, minmod], [majres, majmod], 
-            [np.nanmin(np.array([minres, majres])), np.nanmin(np.array([minmod, majmod]))], 
-            [np.nanmax(np.array([minres, majres])), np.nanmax(np.array([minmod, majmod]))],
-            ['bwr', 'viridis'],
-            [0.5, 0.8],
-            ['.residual', '.model']):
-            fig = plt.figure()
-            ax = fig.add_subplot(1, 2, 1)
-            #z = np.where(mask, -(mask.astype('int')), (majobs - majmod) / majsig)
-            im = ax.pcolormesh(self.x, self.v, zmaj, cmap=cmap, 
-                vmin = vmin, vmax = vmax,
+
+        # plot
+        figs = self.plot_pvds(color = 'model', contour = 'obs',
+            filename = filename, incl = incl, vmask = vmask, pa_maj = pa_maj, pa_min = pa_min,
+            linewidth = linewidth, signmajor = signmajor, signminor = signminor,
+            show = show, set_title = True, nsubgrid = nsubgrid, n_nest = n_nest, reslim = reslim)
+
+
+    def writeout_fitres(self, fout:str = 'PVsilhouette.popt'):
+        '''
+        Write out fitting result.
+
+        Parameters
+        ----------
+        fout (str): Output file name without the file extension.
+        '''
+        if hasattr(self, 'popt'):
+            labels = np.array(['Mstar', 'Rc', 'alpha', 
+                'fflux', 'log_ftau', 'log_frho', 'sig_mdl'])
+            header = '# popt plow pmid phigh\n'
+
+            with open(fout+'.txt', 'w+') as f:
+                f.write(header)
+                np.savetxt(f, 
+                    np.array([labels, self.popt, self.plow, self.pmid, self.phigh]).T,
+                    fmt = '%s')#, '%13.6e', '%13.6e', '%13.6e', '%13.6e'])
+        else:
+            print('ERROR\twriteout_fitres: No optimized parameters are found.')
+            print('ERROR\twriteout_fitres: Run fitting first.')
+            return 0
+
+
+    def read_fitres(self, f: str):
+        '''
+        Read fitting result.
+
+        Parameter
+        ---------
+        f (str): Path to a file containing the fitting result.
+        '''
+        self.popt, self.plow, self.pmid, self.phigh = \
+        np.genfromtxt(f, dtype = float, comments = '#', unpack = True,
+            usecols = (1,2,3,4))
+
+
+    def plot_pvds(self, filename = 'PVsilhouette', 
+        color = 'model', contour = 'obs', incl = 89.,
+        vmask = [0., 0.], pa_maj = None, pa_min = None, linewidth = None,
+        signmajor: int = None, signminor: int = None, 
+        cmap = 'viridis', cmap_residual = 'bwr', ext = '.png',
+        set_title = False,
+        show = False, nsubgrid = 1, n_nest = [3, 3], reslim = 5,
+        shadecolor = 'white', clevels = None):
+        '''
+        Plot observed and model PV diagrams.
+        '''
+
+        # data
+        majobs, minobs = self.dpvmajor.copy(), self.dpvminor.copy()
+
+        # get quadrant
+        majquad = getquad(self.dpvmajor) if signmajor is None else signmajor
+        minquad = getquad(self.dpvminor) * (-1) if signminor is None else signminor
+        obsmax = max(np.nanmax(majobs), np.nanmax(minobs))
+
+        # model
+        mpvd = MockPVD(self.x, self.x, self.v, 
+            nsubgrid = nsubgrid, nnest = n_nest, 
+            beam = self.beam, reslim = reslim)
+        rout = np.nanmax(self.x)
+        def makemodel(Mstar, Rc, alphainfall, fflux, log_ftau, log_frho):
+            major, minor = mpvd.generate_mockpvd(
+                Mstar, Rc, alphainfall, 
+                fflux = fflux * obsmax, frho = 10.**log_frho, ftau = 10.**log_ftau,
+                incl = incl, withkepler = True, linewidth = linewidth,
+                rout = rout, pa = [pa_maj, pa_min],
+                axis = 'both')
+            # quadrant
+            major = major[:, ::majquad] #[:,step//2::step]
+            minor = minor[:, ::minquad] #[:,step//2::step]
+            return major, minor
+
+
+        # grid/data/mask/sigma
+        x, v = np.meshgrid(self.x, self.v)
+        mask = (vmask[0] < v) * (v < vmask[1]) # velocity mask
+        majsig, minsig = self.sigma, self.sigma
+
+        if 'model' in [color, contour]:
+            # check if fitting result exists
+            if hasattr(self, 'popt'):
+                pass
+            else:
+                print('ERROR\twriteout_fitres: No optimized parameters are found.')
+                print('ERROR\twriteout_fitres: Run fitting or read fitting result first.')
+                return 0
+            # model pv diagrams
+            majmod, minmod = makemodel(*self.popt[:-1])
+            # residual
+            majres = np.where(mask, -(mask.astype('int')), (majobs - majmod) / majsig)
+            minres = np.where(mask, -(mask.astype('int')), (minobs - minmod) / minsig)
+            plot_residual = True
+            outlabel = '.model'
+        else:
+            plot_residual = False
+            outlabel = '.obs'
+
+
+        if clevels is None: clevels = np.arange(1, 10) * 3 * self.sigma
+        def makeplots(data_color, data_contour, cmap,
+            vmin = None, vmax = None, vmask = None, alpha = 1.):
+            # set figure
+            fig, axes = plt.subplots(1, 2,)
+            ax1, ax2 = axes
+
+            # major
+            im = ax1.pcolormesh(self.x, self.v, data_color[0], 
+                cmap=cmap, vmin = vmin, vmax = vmax,
                 alpha = alpha, rasterized = True)
-            cax = ax.inset_axes([1.0, 0., 0.05, 1.]) # x0, y0, dx, dy
-            fig.colorbar(im, cax=cax)
-            ax.contour(self.x, self.v, self.dpvmajor,
-                       levels=np.arange(1, 10) * 3 * self.sigma, colors='k')
-            #ax.plot(self.x * majquad, a['major']['vlosmax'], '-r')
-            #ax.plot(self.x * majquad, a['major']['vlosmin'], '-r')
-            ax.set_xlabel('major offset (au)')
-            ax.set_ylabel(r'$V-V_{\rm sys}$ (km s$^{-1}$)')
-            ax.set_ylim(np.min(self.v), np.max(self.v))
-            ax = fig.add_subplot(1, 2, 2)
-            #z = np.where(mask, -(mask.astype('int')), (minobs - minmod) / minsig)
-            im = ax.pcolormesh(self.x, self.v, zmin, cmap=cmap, 
+            #cax = ax1.inset_axes([1.0, 0., 0.05, 1.]) # x0, y0, dx, dy
+            #fig.colorbar(im, cax=cax)
+            ax1.contour(self.x, self.v, data_contour[0],
+                       levels = clevels, colors='k')
+            ax1.set_xlabel('Major offset (au)')
+            ax1.set_ylabel(r'$V-V_{\rm sys}$ (km s$^{-1}$)')
+
+            # minor
+            im = ax2.pcolormesh(self.x, self.v, data_color[1], cmap=cmap, 
                 vmin= vmin, vmax = vmax, alpha=alpha, rasterized=True)
-            ax.contour(self.x, self.v, self.dpvminor,
-                       levels=np.arange(1, 10) * 3 * self.sigma, colors='k')
-            cax = ax.inset_axes([1.0, 0., 0.05, 1.]) # x0, y0, dx, dy
-            fig.colorbar(im, cax=cax)
-            #ax.plot(self.x * minquad, a['minor']['vlosmax'], '-r')
-            #ax.plot(self.x * minquad, a['minor']['vlosmin'], '-r')
-            ax.set_xlabel('minor offset (au)')
-            ax.set_ylim(self.v.min(), self.v.max())
-            ax.set_title(r'$M_{*}$'+f'={popt[0]:.2f}'+r'$M_{\odot}$'
-                +', '+r'$R_{c}$'+f'={popt[1]:.0f} au'
-                +'\n'+r'$\alpha$'+f'={popt[2]:.2f}'
-                +', '+r'$\alpha ^{2} M_{*}$'+f'={popt[0] * popt[2]**2:.2}')
-            fig.tight_layout()
-            fig.savefig(figname + lbl + '.png')
+            ax2.contour(self.x, self.v, data_contour[1],
+                       levels = clevels, colors='k')
+            cax2 = ax2.inset_axes([1.0, 0., 0.05, 1.]) # x0, y0, dx, dy
+            fig.colorbar(im, cax=cax2)
+            ax2.set_yticklabels('')
+            ax2.set_xlabel('Minor offset (au)')
+
+            for ax in axes:
+                ax.set_xlim(np.min(self.x), np.max(self.x))
+                ax.set_ylim(np.min(self.v), np.max(self.v))
+
+            if vmask is not None:
+                for ax in axes:
+                    ax.fill_between(
+                        self.x, np.full(len(self.x), vmask[0]), np.full(len(self.x), vmask[1]),
+                        color = shadecolor, alpha = 0.6,
+                        edgecolor = None,
+                        )
+
+            if set_title:
+                ax2.set_title(r'$M_{*}$'+f'={self.popt[0]:.2f}'+r'$M_{\odot}$'
+                    +', '+r'$R_{c}$'+f'={self.popt[1]:.0f} au'
+                    +'\n'+r'$\alpha$'+f'={self.popt[2]:.2f}'
+                    +', '+r'$\alpha ^{2} M_{*}$'+f'={self.popt[0] * self.popt[2]**2:.2}')
+
+            return fig
+
+
+        # color images and model images
+        d_col = [majmod, minmod] if color == 'model' else [majobs, minobs]
+        d_con = [majmod, minmod] if contour == 'model' else [majobs, minobs]
+
+
+        figs = []
+        # main plot
+        vmin, vmax = np.nanmin(np.array(d_col)), np.nanmax(np.array(d_col))
+        vmask = vmask if vmask[1] > vmask[0] else None
+        fig = makeplots(d_col, d_con, cmap, vmin = vmin, vmax = vmax, 
+            vmask = vmask, alpha = 0.8)
+        fig.tight_layout()
+        fig.savefig(filename + outlabel + ext, dpi = 300)
+        figs.append(fig)
+        if show: plt.show()
+
+        # residual plot
+        if plot_residual:
+            d_col = [majres, minres] if color == 'model' else [majobs, minobs]
+            d_con = [majres, minres] if contour == 'model' else [majobs, minobs]
+            vmin, vmax = np.nanmin(np.array(d_col)), np.nanmax(np.array(d_col))
+            # plot
+            fig2 = makeplots(d_col, d_con, cmap_residual, 
+                vmin = vmin, vmax = vmax, 
+                vmask = None, alpha = 0.5)
+            fig2.tight_layout()
+            fig2.savefig(filename + '.residual' + ext, dpi = 300)
+            figs.append(fig2)
             if show: plt.show()
+
+        return figs
+
+
+def getquad(m):
+    '''
+    Get quadrant
+    '''
+    nv, nx = np.shape(m)
+    q = np.sum(m[:nv//2, :nx//2]) + np.sum(m[nv//2:, nx//2:]) \
+        - np.sum(m[nv//2:, :nx//2]) - np.sum(m[:nv//2, nx//2:])
+    return int(np.sign(q))
