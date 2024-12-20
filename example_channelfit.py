e7ac2d4d2b3926fd9d22ee8705ca2bc092688b1e
Yusuke Aso <98569855+yusukeaso-astron@users.noreply.github.com>
2024-12-20 17:10:49 +0900

    update the example of channelfit

--- example_channelfit.py
+++ example_channelfit.py
@@ -1,15 +1,15 @@
 from channelfit import ChannelFit
 
 '-------- INPUTS --------'
-cubefits = './channelfit/IRAS16253_SBLB_C18O_robust_2.0.imsub.fits'
-center = '16h28m21.61526785s -24d36m24.32538414s'
-pa = 113 - 180  # deg
-incl = 65  # deg
-vsys = 4  # km/s
-dist = 139  # pc
-sigma = 2e-3  # Jy/beam; None means automatic calculation.
-rmax = 1 * dist  # au
-vlim = (-2.7, -1.0, 1.0, 2.7)  # km/s; from vsys
+cubefits = './channelfit/test3D.fits'
+center = '04h04m43.07s 26d18m56.20s'
+pa = 90  # deg
+incl = 45  # deg
+vsys = 0  # km/s
+dist = 140  # pc
+sigma = 1.5e-3  # Jy/beam; None means automatic calculation.
+rmax = 0.5 * dist  # au
+vlim = (-5.0, -3.0, 3.0, 5.0)  # km/s; from vsys
 '------------------------'
 
 
@@ -18,17 +18,16 @@ if __name__ == '__main__':
     filehead = cubefits.replace('.fits', '')
     chan = ChannelFit(scaling='mom0ft', progressbar=True)
     chan.makegrid(cubefits=cubefits, center=center, pa=pa, incl=incl,
-                  vsys=vsys, dist=dist, sigma=sigma, rmax=rmax, vlim=vlim,
-                  skipto=5)
-    chan.fitting(Mstar_range=[0.01, 1.0],
-                 Rc_range=[3, 300],
-                 fixed_params={'cs': 0.4, 'h1': 0, 'h2': -1, 'Rin': 0,
+                  vsys=vsys, dist=dist, sigma=sigma, rmax=rmax, vlim=vlim)
+    chan.fitting(Mstar_range=[0.3, 3.0],
+                 cs_range=[0.0, 0.3],
+                 fixed_params={'Rc': 200, 'h1': 0, 'h2': -1, 'Rin': 0,
                                'pI': 0, 'Ienv': 0,
                                'xoff': 0, 'yoff': 0, 'voff': 0, 'incl': 0},
-                 kwargs_emcee_corner={'nwalkers_per_ndim': 2,
-                                      'nburnin': 100,
+                 kwargs_emcee_corner={'nwalkers_per_ndim': 8,
+                                      'nburnin': 300,
                                       'nsteps': 300,
-                                      'rangelevel': 0.9},
+                                      'rangelevel': 0.99},
                  filename=filehead)
     p = chan.popt
     chan.modeltofits(**p, filehead=filehead)
