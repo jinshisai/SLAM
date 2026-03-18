a893c3a000c56895dc20824d31e287040e646728
Yusuke Aso <98569855+yusukeaso-astron@users.noreply.github.com>
2026-03-18 14:59:34 +0900

    ruff to _channelfit

--- channelfit/_channelfit.py
+++ channelfit/_channelfit.py
@@ -337,7 +337,7 @@ class ChannelFit(ReadFits):
         xb = (np.arange(2 * ngauss + 1) - ngauss) * dpix
         yb = (np.arange(2 * ngauss + 1) - ngauss) * dpix
         xb, yb = rot(*np.meshgrid(xb, yb), np.radians(self.bpa))
-        gaussbeam = np.exp2(-4 *((yb / self.bmaj)**2 + (xb / self.bmin)**2))
+        gaussbeam = np.exp2(-4 * ((yb / self.bmaj)**2 + (xb / self.bmin)**2))
         self.pixperbeam = np.sum(gaussbeam)
         self.gaussbeam = gaussbeam  # The 1st (x) axis is in the model order.
 
