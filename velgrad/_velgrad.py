f6e816057ae710e9e9a384233862ffc7101accb7
Yusuke Aso <98569855+yusukeaso-astron@users.noreply.github.com>
2026-06-19 22:49:24 +0900

    dof without -1

--- velgrad/_velgrad.py
+++ velgrad/_velgrad.py
@@ -301,7 +301,7 @@ class VelGrad(ReadFits):
             if voff_fixed is not None:
                 plim = plim[:, :-1]
             popt, perr = emcee_custom(plim, lnprob, False)
-            dof = len(v) - len(popt) - 1
+            dof = len(v) - len(popt)
             self.chi2r_mass = -2. * lnprob(popt) / dof
             if voff_fixed is not None:
                 popt = np.r_[popt, 0]
