81314bd3ae44835c932aa74ec24326d00e8af144
Yusuke Aso <98569855+yusukeaso-astron@users.noreply.github.com>
2026-08-06 16:51:22 +0900

    tabs to spaces

--- pvanalysis/analysis_tools.py
+++ pvanalysis/analysis_tools.py
@@ -92,27 +92,27 @@ def doublepower_r_error(v, r_break, v_break, p_in, dp, vsys,
 
 # read file
 def read_pvfitres(fname, inner_threshold=None, outer_threshold=None, toau=False, dist=140.):
-	# read files
-	offset, offerr, velocity, velerr = np.genfromtxt(fname, comments='#', unpack=True)
+    # read files
+    offset, offerr, velocity, velerr = np.genfromtxt(fname, comments='#', unpack=True)
 
-	# offset threshold of used data point
-	if inner_threshold:
-		thrindx = np.where(np.abs(offset) >= inner_threshold)
-		offset = offset[thrindx]
-		velocity = velocity[thrindx]
-		offerr = offerr[thrindx]
-		velerr = velerr[thrindx]
+    # offset threshold of used data point
+    if inner_threshold:
+        thrindx = np.where(np.abs(offset) >= inner_threshold)
+        offset = offset[thrindx]
+        velocity = velocity[thrindx]
+        offerr = offerr[thrindx]
+        velerr = velerr[thrindx]
 
-	# offset threshold of used data point
-	if outer_threshold:
-		thrindx = np.where(np.abs(offset) <= outer_threshold)
-		offset = offset[thrindx]
-		velocity = velocity[thrindx]
-		offerr = offerr[thrindx]
-		velerr = velerr[thrindx]
+    # offset threshold of used data point
+    if outer_threshold:
+        thrindx = np.where(np.abs(offset) <= outer_threshold)
+        offset = offset[thrindx]
+        velocity = velocity[thrindx]
+        offerr = offerr[thrindx]
+        velerr = velerr[thrindx]
 
-	if toau:
-		offset = offset*dist
-		offerr = offerr*dist
+    if toau:
+        offset = offset*dist
+        offerr = offerr*dist
 
-	return offset, velocity, offerr, velerr
+    return offset, velocity, offerr, velerr
--- pvanalysis/fitfuncs.py
+++ pvanalysis/fitfuncs.py
@@ -14,24 +14,24 @@ from scipy.stats import norm
 # functions
 # gaussian
 def gauss1d(x, amp, mean, sig):
-	return amp * np.exp(-(x-mean)*(x-mean)/(2.0*sig*sig))
+    return amp * np.exp(-(x-mean)*(x-mean)/(2.0*sig*sig))
 
 
 # for chi-square
 def chi_gauss1d(param, xdata, ydata, ysig):
-	return (ydata - (gauss1d(xdata, *param))) / ysig
+    return (ydata - (gauss1d(xdata, *param))) / ysig
 
 
 def edge(xdata, ydata, yerr, threshold, goodflag=None, edgesign=1):
-	grad = (np.roll(ydata, -1) - np.roll(ydata, 1)) / (xdata[2] - xdata[0])
-	cond = (ydata > threshold) if goodflag is None else (ydata > threshold) * goodflag
-	grad, x = grad[cond], xdata[cond]
-	if len(x) == 0:
-		return [np.nan, np.nan]
-	val = x[0] if edgesign < 0 else x[-1]
-	grad = grad[0] if edgesign < 0 else grad[-1]
-	err = yerr / np.abs(grad)
-	return [val, err]
+    grad = (np.roll(ydata, -1) - np.roll(ydata, 1)) / (xdata[2] - xdata[0])
+    cond = (ydata > threshold) if goodflag is None else (ydata > threshold) * goodflag
+    grad, x = grad[cond], xdata[cond]
+    if len(x) == 0:
+        return [np.nan, np.nan]
+    val = x[0] if edgesign < 0 else x[-1]
+    grad = grad[0] if edgesign < 0 else grad[-1]
+    err = yerr / np.abs(grad)
+    return [val, err]
 
 
 '''
@@ -52,52 +52,52 @@ def ridge_gauss(xdata, ydata, yerr):
 
 
 def gaussfit(xdata, ydata, yerr):
-	'''
+    '''
 	Gaussian fit through chi-square fit.
 	'''
 
-	# Get estimate of the initial parameters
-	indx_pini = ydata >= 3.*yerr
-	mx = np.nansum(ydata[indx_pini]*xdata[indx_pini])/np.nansum(ydata[indx_pini])  # weighted mean
-	sigx = np.sqrt(np.nansum(ydata[indx_pini]*(xdata[indx_pini] - mx)**2.)/np.nansum(ydata[indx_pini]))  # standerd deviation
+    # Get estimate of the initial parameters
+    indx_pini = ydata >= 3.*yerr
+    mx = np.nansum(ydata[indx_pini]*xdata[indx_pini])/np.nansum(ydata[indx_pini])  # weighted mean
+    sigx = np.sqrt(np.nansum(ydata[indx_pini]*(xdata[indx_pini] - mx)**2.)/np.nansum(ydata[indx_pini]))  # standerd deviation
 
-	# if sigx is too small
-	if sigx <= 1e-6:
-		sigx = np.abs(xdata[1] - xdata[0])/len(xdata)
+    # if sigx is too small
+    if sigx <= 1e-6:
+        sigx = np.abs(xdata[1] - xdata[0])/len(xdata)
 
-	amp = np.nanmax(ydata)
-	pinp = [amp, mx, sigx]
+    amp = np.nanmax(ydata)
+    pinp = [amp, mx, sigx]
 
-	if len(xdata) < 3:
-		param_out = np.full(3, np.nan)
-		param_err = np.full(3, np.nan)
-		return param_out, param_err
+    if len(xdata) < 3:
+        param_out = np.full(3, np.nan)
+        param_err = np.full(3, np.nan)
+        return param_out, param_err
 
-	# fitting
-	results = optimize.leastsq(chi_gauss1d, pinp, args=(xdata, ydata, yerr), full_output=True)
-	param_out = results[0]
-	param_cov = results[1]
-	# print(results)
-	# print(param_out, param_cov)
-	# Do not multiply covariance by reduced chi^2 to obtain absolute errors
+    # fitting
+    results = optimize.leastsq(chi_gauss1d, pinp, args=(xdata, ydata, yerr), full_output=True)
+    param_out = results[0]
+    param_cov = results[1]
+    # print(results)
+    # print(param_out, param_cov)
+    # Do not multiply covariance by reduced chi^2 to obtain absolute errors
 
-	# parameter error estimates
-	if param_cov is not None:
-		param_err = np.array([
-			np.abs(param_cov[j][j])**0.5 for j in range(len(pinp))
-			])
-	else:
-		param_err = np.full(3, np.nan)
-		param_out = np.full(3, np.nan) if (param_out == pinp).all else param_out
+    # parameter error estimates
+    if param_cov is not None:
+        param_err = np.array([
+            np.abs(param_cov[j][j])**0.5 for j in range(len(pinp))
+            ])
+    else:
+        param_err = np.full(3, np.nan)
+        param_out = np.full(3, np.nan) if (param_out == pinp).all else param_out
 
-	# print results
-	# print ('Chi2: ', reduced_chi2)
-	# print ('Fitting results: amp   mean   sigma')
-	# print (amp_fit, mx_fit, sigx_fit)
-	# print ('Errors')
-	# print (amp_fit_err, mx_fit_err, sigx_fit_err)
+    # print results
+    # print ('Chi2: ', reduced_chi2)
+    # print ('Fitting results: amp   mean   sigma')
+    # print (amp_fit, mx_fit, sigx_fit)
+    # print ('Errors')
+    # print (amp_fit_err, mx_fit_err, sigx_fit_err)
 
-	return param_out, param_err
+    return param_out, param_err
 
 
 def ridge_mean(xdata, ydata, yerr):
@@ -110,7 +110,7 @@ def ridge_mean(xdata, ydata, yerr):
 
 # functions
 def splaw(r, params, r0=100.):
-	'''
+    '''
 	Single power-law function.
 
 	Args:
@@ -118,26 +118,26 @@ def splaw(r, params, r0=100.):
 	 - params: [vsys, v0, p]
 	 - r0: 100 (au)
 	'''
-	vsys, v0, p = params
-	vout = v0*(r/r0)**(-p)
-	dydx = (v0/r0)*(-p)*(r/r0)**(-p-1)
-	return vout, dydx
+    vsys, v0, p = params
+    vout = v0*(r/r0)**(-p)
+    dydx = (v0/r0)*(-p)*(r/r0)**(-p-1)
+    return vout, dydx
 
 
 def chi_splaw(params, xdata, ydata, xsig, ysig):
-	'''
+    '''
 	Calculate chi for chi-square for the single power-law function.
 	'''
-	vsys = params[0]
-	vout, dydx = splaw(xdata, params)
-	# sig       = np.sqrt((xsig*dydx)*(xsig*dydx) + ysig*ysig)
-	sig = ysig
-	chi_out = (np.abs(ydata - vsys) - vout)/sig
-	return chi_out
+    vsys = params[0]
+    vout, dydx = splaw(xdata, params)
+    # sig       = np.sqrt((xsig*dydx)*(xsig*dydx) + ysig*ysig)
+    sig = ysig
+    chi_out = (np.abs(ydata - vsys) - vout)/sig
+    return chi_out
 
 
 def dplaw(radii, params):
-	'''
+    '''
 	Double power-law function.
 
 	Args:
@@ -148,22 +148,22 @@ def dplaw(radii, params):
 	  - pin: power at r < rb
 	  - pout: power at r >= rb
 	'''
-	vb, rb, pin, pout = params
+    vb, rb, pin, pout = params
 
-	vout = np.array([
-		vb*(r/rb)**(-pin) if r < rb else vb*(r/rb)**(-pout)
-		for r in radii
-		])
-	dydx = np.array([
-		(vb/rb)*(-pin)*(r/rb)**(-pin-1) if r < rb else (vb/rb)*(-pout)*(r/rb)**(-pout-1)
-		for r in radii
-		])
+    vout = np.array([
+        vb*(r/rb)**(-pin) if r < rb else vb*(r/rb)**(-pout)
+        for r in radii
+        ])
+    dydx = np.array([
+        (vb/rb)*(-pin)*(r/rb)**(-pin-1) if r < rb else (vb/rb)*(-pout)*(r/rb)**(-pout-1)
+        for r in radii
+        ])
 
-	return vout, dydx
+    return vout, dydx
 
 
 def chi_dplaw(params, xdata, ydata, xsig, ysig):
-	'''
+    '''
 	Chi for chi square for the double power-law function.
 
 	Args:
@@ -174,69 +174,69 @@ def chi_dplaw(params, xdata, ydata, xsig, ysig):
 	 - xsig: sigma for x
 	 - ysig: sigma for y
 	'''
-	vout, dydx = dplaw(xdata, params)
-	# sig       = np.sqrt((xsig*dydx)*(xsig*dydx) + ysig*ysig)
-	sig = ysig
-	chi_out = (np.abs(ydata - vout))/sig
-	return chi_out
+    vout, dydx = dplaw(xdata, params)
+    # sig       = np.sqrt((xsig*dydx)*(xsig*dydx) + ysig*ysig)
+    sig = ysig
+    chi_out = (np.abs(ydata - vout))/sig
+    return chi_out
 
 
 def estimate_perror(params, func, x, y, xerr, yerr, niter=3000):
-	'''
+    '''
 	Estimate fitting-parameter errors by Monte-Carlo method.
 
 	'''
-	nparams = len(params)
-	perrors = np.zeros((0, nparams), float)
+    nparams = len(params)
+    perrors = np.zeros((0, nparams), float)
 
-	for i in range(niter):
-		offest = norm.rvs(size=len(x), loc=x, scale=xerr)
-		velest = norm.rvs(size=len(y), loc=y, scale=yerr)
-		result = optimize.leastsq(func, params, args=(offest, velest, xerr, yerr), full_output=True)
-		perrors = np.vstack((perrors, result[0]))
-		# print param_esterr[:,0]
+    for i in range(niter):
+        offest = norm.rvs(size=len(x), loc=x, scale=xerr)
+        velest = norm.rvs(size=len(y), loc=y, scale=yerr)
+        result = optimize.leastsq(func, params, args=(offest, velest, xerr, yerr), full_output=True)
+        perrors = np.vstack((perrors, result[0]))
+        # print param_esterr[:,0]
 
-	sigmas = np.array([
-		np.std(perrors[:, i]) for i in range(nparams)
-		])
-	medians = np.array([
-		np.median(perrors[:, i]) for i in range(nparams)
-		])
+    sigmas = np.array([
+        np.std(perrors[:, i]) for i in range(nparams)
+        ])
+    medians = np.array([
+        np.median(perrors[:, i]) for i in range(nparams)
+        ])
 
-	with np.printoptions(precision=4, suppress=True):
-		print('Estimated errors (standard deviation):')
-		print(sigmas)
-		print('Medians:')
-		print(medians)
+    with np.printoptions(precision=4, suppress=True):
+        print('Estimated errors (standard deviation):')
+        print(sigmas)
+        print('Medians:')
+        print(medians)
 
-	# plot the Monte-Carlo results
-	fig_errest = plt.figure(figsize=(11.69, 8.27), frameon=False)
-	gs = GridSpec(nparams, 2)
-	if nparams == 3:
-		xlabels = [r'$V_\mathrm{sys}$', r'$V_\mathrm{100}$', r'$p$']
-	elif nparams == 4:
-		xlabels = [r'$V_\mathrm{break}$', r'$R_\mathrm{break}$', r'$p_\mathrm{in}$', r'$p_\mathrm{out}$']
-	else:
-		xlabels = [r'$p%i$' % (i+1) for i in range(nparams)]
+    # plot the Monte-Carlo results
+    fig_errest = plt.figure(figsize=(11.69, 8.27), frameon=False)
+    gs = GridSpec(nparams, 2)
+    if nparams == 3:
+        xlabels = [r'$V_\mathrm{sys}$', r'$V_\mathrm{100}$', r'$p$']
+    elif nparams == 4:
+        xlabels = [r'$V_\mathrm{break}$', r'$R_\mathrm{break}$', r'$p_\mathrm{in}$', r'$p_\mathrm{out}$']
+    else:
+        xlabels = [r'$p%i$' % (i+1) for i in range(nparams)]
 
-	for i in range(nparams):
-		# histogram
-		ax1 = fig_errest.add_subplot(gs[i, 0])
-		ax1.set_xlabel(xlabels[i])
-		if i == (nparams - 1):
-			ax1.set_ylabel('Frequency')
-		ax1.hist(perrors[:, i], bins=50, cumulative=False)  # density = True
+    for i in range(nparams):
+        # histogram
+        ax1 = fig_errest.add_subplot(gs[i, 0])
+        ax1.set_xlabel(xlabels[i])
+        if i == (nparams - 1):
+            ax1.set_ylabel('Frequency')
+        ax1.hist(perrors[:, i], bins=50, cumulative=False)  # density = True
 
-		# cumulative histogram
-		ax2 = fig_errest.add_subplot(gs[i, 1])
-		ax2.set_xlabel(xlabels[i])
-		if i == (nparams - 1):
-			ax2.set_ylabel('Cumulative\n frequency')
-		ax2.hist(perrors[:, i], bins=50, density=True, cumulative=True)
+        # cumulative histogram
+        ax2 = fig_errest.add_subplot(gs[i, 1])
+        ax2.set_xlabel(xlabels[i])
+        if i == (nparams - 1):
+            ax2.set_ylabel('Cumulative\n frequency')
+        ax2.hist(perrors[:, i], bins=50, density=True, cumulative=True)
 
-	plt.subplots_adjust(wspace=0.4, hspace=0.4)
-	# plt.show()
-	fig_errest.savefig('errest.pdf', transparent=True)
-	fig_errest.clf()
+    plt.subplots_adjust(wspace=0.4, hspace=0.4)
+    # plt.show()
+    fig_errest.savefig('errest.pdf', transparent=True)
+    fig_errest.clf()
 
-	return sigmas
+    return sigmas
