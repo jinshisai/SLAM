8722608e6c171c894b354987e49988ac4c90a8a9
Yusuke Aso <98569855+yusukeaso-astron@users.noreply.github.com>
2026-08-06 17:24:36 +0900

    {d:d} to {d}

--- channelfit/_channelfit.py
+++ channelfit/_channelfit.py
@@ -765,8 +765,8 @@ class ChannelFit(ReadFits):
             print('Parameter values (opt, low, mid, high):')
             for i, (k, d, u) in enumerate(zip(self.paramkeys, digits, ulist)):
                 p = [self.popt[i], self.plow[i], self.pmid[i], self.phigh[i]]
-                print(f'{k} = {p[0]:.{d:d}f}, {p[1]:.{d:d}f},'
-                      + f' {p[2]:.{d:d}f}, {p[3]:.{d:d}f} {u}')
+                print(f'{k} = {p[0]:.{d}f}, {p[1]:.{d}f},'
+                      + f' {p[2]:.{d}f}, {p[3]:.{d}f} {u}')
         if runfit and save_result:
             plist = [self.popt, self.plow, self.pmid, self.phigh]
             with open(filename+'.popt.txt', 'w') as f:
--- pvfitting/_pvfitting.py
+++ pvfitting/_pvfitting.py
@@ -257,8 +257,8 @@ class PVFitting(ReadFits):
             print('Parameter values (opt, low, mid, high):')
             for i, (k, d, u, f) in enumerate(zip(paramkeys, digits, ulist, flist)):
                 p = [self.popt[i], self.plow[i], self.pmid[i], self.phigh[i]]
-                print(f'{k} = {p[0]:.{d:d}{f}}, {p[1]:.{d:d}{f}},'
-                      + f' {p[2]:.{d:d}{f}}, {p[3]:.{d:d}{f}} {u}')
+                print(f'{k} = {p[0]:.{d}{f}}, {p[1]:.{d}{f}},'
+                      + f' {p[2]:.{d}{f}}, {p[3]:.{d}{f}} {u}')
         if runfit and save_result:
             plist = [self.popt, self.plow, self.pmid, self.phigh]
             with open(filename+'.popt.txt', 'w') as f:
